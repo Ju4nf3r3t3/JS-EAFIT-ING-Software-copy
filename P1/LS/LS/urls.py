8b8ff881d27ec80ce269127e45a6a451ec63c768
josecardenasl <111715270+josecardenasl@users.noreply.github.com>
2025-03-02 15:21:36 -0500

    subiendo cambios a mi rama

--- P1/LS/LS/urls.py
+++ P1/LS/LS/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from login.views import login_view
+from login.views import login_view, home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', login_view, name='login'),
+    path("home/", home_view, name="home"),  
 ]
--- P1/LS/login/views.py
+++ P1/LS/login/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.http import HttpResponse
 
 def login_view(request):
-    return render(request, 'login.html')
+    context = {}  # Diccionario para enviar datos al template
+    
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Inicio de sesión exitoso")
+            return redirect("home")  # Redirige a la página principal
+        else:
+            messages.error(request, "Usuario o contraseña incorrectos")
+
+    return render(request, "login.html")
+
+
+def home_view(request):
+    return HttpResponse("<h1>Bienvenido, has iniciado sesión correctamente</h1>")
+
